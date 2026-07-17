543ef34a762ec088269d3b4f735b1a071e5ff3ac
Harsh Singla <imharshsingla@gmail.com>
2026-07-17 15:24:59 +0530

    Time: 0 ms (100.00%) | Memory: 44.1 MB (8.73%) - LeetSync

--- 74-search-a-2d-matrix/search-a-2d-matrix.java
+++ 74-search-a-2d-matrix/search-a-2d-matrix.java
@@ -1,39 +1,72 @@
 class Solution {
     public boolean searchMatrix(int[][] matrix, int target) {
 
-        int m = matrix.length;
-        int n = matrix[0].length;
-        int a =0;
+        // 1st Approach - with extra space cuz array introduced
 
-        int[] arr = new int[m*n];
+        // int m = matrix.length;
+        // int n = matrix[0].length;
+        // int a =0;
 
-        for(int i=0;i<m;i++){
-            for(int j=0;j<n;j++){
-                arr[a] = matrix[i][j];
-                a++;
-            }
-        }
+        // int[] arr = new int[m*n];
 
-        int left=0;
-        int right = arr.length-1;
+        // for(int i=0;i<m;i++){
+        //     for(int j=0;j<n;j++){
+        //         arr[a] = matrix[i][j];
+        //         a++;
+        //     }
+        // }
+
+        // int left=0;
+        // int right = arr.length-1;
+
+        // while(left<=right){
+        //     int mid = left + (right-left)/2;
+
+        //     if(arr[mid]==target){
+        //         return true;
+        //     }
+
+        //     if(target>arr[mid]){
+        //         left = mid+1;
+        //     }
+        //     else{
+        //         right = mid-1;
+        //     }
+        // }
+
+        // return false;
+
+
+        // 2nd approach
+
+        int n = matrix.length;
+        int m = matrix[0].length;
+
+        int left =0;
+        int right = m*n -1;
+
 
         while(left<=right){
-            int mid = left + (right-left)/2;
 
-            if(arr[mid]==target){
+            int mid = left + (right-left)/2;
+            int row = mid/m;
+            int col = mid%m;
+
+            if(matrix[row][col] == target){
                 return true;
             }
 
-            if(target>arr[mid]){
-                left = mid+1;
-            }
-            else{
+            if(matrix[row][col]>target){
                 right = mid-1;
             }
+            else{
+                left = mid+1;
+            }
+
+
         }
 
         return false;
-
         
     }
 }
