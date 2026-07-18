0366ad290f6e47d00ff8e7b0df5f64526edc92d8
Harsh Singla <imharshsingla@gmail.com>
2026-07-18 17:02:16 +0530

    Time: 0 ms (100.00%) | Memory: 48.2 MB (51.58%) - LeetSync

--- 34-find-first-and-last-position-of-element-in-sorted-array/find-first-and-last-position-of-element-in-sorted-array.java
+++ 34-find-first-and-last-position-of-element-in-sorted-array/find-first-and-last-position-of-element-in-sorted-array.java
@@ -1,46 +1,105 @@
 class Solution {
     public int[] searchRange(int[] nums, int target) {
         
-        int left =0;
+        // 1st Approach: TC - O(n) -> not optimal
+        // int left =0;
+        // int n = nums.length;
+        // int right = n-1;
+
+        // int[] ans = new int[2];
+        // int min = -1;
+        // int max = -1;
+
+        // while(left<=right){
+        //     int mid = left + (right-left)/2;
+
+        //     if(nums[mid]==target){
+        //         min = mid;
+        //         max = mid;
+
+        //         while(min >0 && min<=max && nums[min-1]==target){
+        //             min--;
+        //         }
+        //         while(max <n-1 && max>=min && nums[max+1]==target){
+        //             max++;
+        //         }
+        //         break;
+        //     }
+
+        //     if(nums[mid]>target){
+        //         right = mid-1;
+        //     }
+        //     else{
+        //         left = mid+1;
+        //     }
+        // }
+        // ans[0] = min;
+        // ans[1] = max;
+        // return ans;
+
+
+        // 2nd Approach -> TC-O(log(n));
+
         int n = nums.length;
+        int left =0;
         int right = n-1;
 
+        int min =-1;
+        int max =-1;
         int[] ans = new int[2];
-        int min = -1;
-        int max = -1;
 
         while(left<=right){
             int mid = left + (right-left)/2;
 
             if(nums[mid]==target){
                 min = mid;
-                max = mid;
-
-                while(min >0 && min<=max && nums[min-1]==target){
-                    min--;
-                }
-                while(max <n-1 && max>=min && nums[max+1]==target){
-                    max++;
-                }
-
-                break;
-
-
-            }
-
-
-            if(nums[mid]>target){
                 right = mid-1;
+
             }
-            else{
+
+            if(nums[mid]<target){
                 left = mid+1;
             }
+            else{
+                right = mid-1;
+            } 
         }
 
-        ans[0] = min;
-        ans[1] = max;
+        if(min==-1){
+            ans[0] = min;
+            ans[1] = -1;
+            return ans;
+        }
+        else{
+
+            ans[0] = min;
+
+            left = min;
+            right = n-1;
+
+            while(left<=right){
+                int mid = left + (right-left)/2;
+
+                if(nums[mid]==target){
+                    max = mid;
+                    left = mid+1;
+                }
+
+                if(nums[mid]>target){
+                    right = mid-1;
+                }
+                else{
+                    left = mid+1;
+                }
+            }
+
+            ans[1] = max;
+
+
+        }
 
         return ans;
+
 
     }
 }
