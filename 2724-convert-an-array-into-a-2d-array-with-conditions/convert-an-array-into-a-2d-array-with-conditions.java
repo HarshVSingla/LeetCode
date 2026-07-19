9af61901161f17d430f6b05b2bdaa7f922da9b25
Harsh Singla <imharshsingla@gmail.com>
2026-07-19 23:39:11 +0530

    Time: 2 ms (91.37%) | Memory: 46.8 MB (40.16%) - LeetSync

--- 2724-convert-an-array-into-a-2d-array-with-conditions/convert-an-array-into-a-2d-array-with-conditions.java
+++ 2724-convert-an-array-into-a-2d-array-with-conditions/convert-an-array-into-a-2d-array-with-conditions.java
@@ -1,39 +1,58 @@
 class Solution {
     public List<List<Integer>> findMatrix(int[] nums) {
 
+        // 1st approach
+
+        // HashMap<Integer,Integer> map = new HashMap<>();
+
+        // for(int i=0;i<nums.length;i++){
+        //     map.put(nums[i],map.getOrDefault(nums[i],0)+1);
+        // }
+        // int mx =0;
+        // for(Integer num : map.values()){
+        //     mx = Math.max(mx,num);
+        // }
+        // List<List<Integer>> ans = new ArrayList<>();
+
+        // for(int i=0;i<mx;i++){
+
+        //     int size = map.size();
+
+        //     List<Integer> arr = new ArrayList<>();
+
+        //     for(Integer num : new ArrayList<>(map.keySet())){
+        //         arr.add(num);
+        //         map.put(num,map.get(num)-1);
+        //         if(map.get(num)==0){
+        //             map.remove(num);
+        //         }
+        //     }
+
+        //     ans.add(arr);
+        // }
+        // return ans;
+
+
+        // 2nd approach:
+
         HashMap<Integer,Integer> map = new HashMap<>();
-
-        for(int i=0;i<nums.length;i++){
-            map.put(nums[i],map.getOrDefault(nums[i],0)+1);
-        }
-
-        int mx =0;
-        for(Integer num : map.values()){
-            mx = Math.max(mx,num);
-        }
 
         List<List<Integer>> ans = new ArrayList<>();
 
-        for(int i=0;i<mx;i++){
+        for(int num : nums){
 
-            int size = map.size();
+            int count = map.getOrDefault(num,0);
 
-            List<Integer> arr = new ArrayList<>();
-
-            for(Integer num : new ArrayList<>(map.keySet())){
-                arr.add(num);
-                map.put(num,map.get(num)-1);
-                if(map.get(num)==0){
-                    map.remove(num);
-                }
+            if(count==ans.size()){
+                ans.add(new ArrayList<>());
             }
 
-            ans.add(arr);
+            ans.get(count).add(num);
+            map.put(num,count+1);
 
-        }
+        } 
 
         return ans;
-
 
         
     }
