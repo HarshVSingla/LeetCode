c866feeb4e98d4a5e64d8e6084db44fa6446ff8f
Harsh Singla <imharshsingla@gmail.com>
2026-07-20 12:39:40 +0530

    Time: 3 ms (54.63%) | Memory: 50.9 MB (51.70%) - LeetSync

--- 966-binary-subarrays-with-sum/binary-subarrays-with-sum.java
+++ 966-binary-subarrays-with-sum/binary-subarrays-with-sum.java
@@ -1,22 +1,75 @@
 class Solution {
     public int numSubarraysWithSum(int[] nums, int goal) {
 
-        int count =0;
-        int prefix =0;
+        // int count =0;
+        // int prefix =0;
 
-        HashMap<Integer,Integer> map = new HashMap<>();
-        map.put(0,1);
+        // HashMap<Integer,Integer> map = new HashMap<>();
+        // map.put(0,1);
 
-        for(int right =0;right<nums.length;right++){
+        // for(int right =0;right<nums.length;right++){
 
-            prefix+= nums[right];
+        //     prefix+= nums[right];
 
-            count+= map.getOrDefault(prefix-goal,0);
-            map.put(prefix,map.getOrDefault(prefix,0)+1);     
+        //     count+= map.getOrDefault(prefix-goal,0);
+        //     map.put(prefix,map.getOrDefault(prefix,0)+1);     
 
+        // }
+
+        // return count;
+
+
+
+        // 2nd approach - more optimal ig 
+
+        int left =0;
+        int count=0;
+        int countles=0;
+        int sum =0;
+        int n = nums.length;
+
+
+        for(int right =0;right<n;right++){
+
+            sum+= nums[right];
+
+            while(sum>goal){
+                sum-= nums[left];
+                left++;
+                if(left==n){
+                    break;
+                }
+            }
+
+            count+= right-left+1;
+  
+        }
+        sum =0;
+        left=0;
+
+        if(goal==0){
+            countles =0;
+        }
+        else{
+
+            for(int right =0;right<n;right++){
+            sum+= nums[right];
+
+            while(sum>=goal){
+                sum-= nums[left];
+                left++;
+                if(left==n){
+                    break;
+                }
+            }
+
+            countles+= right-left+1;
         }
 
-        return count;
+
+        }
+        
+        return count-countles; 
         
     }
 }
