992f8c6ec2be23dde8e8362588be3e92707de0ae
Harsh Singla <imharshsingla@gmail.com>
2026-07-21 23:56:54 +0530

    Time: 41 ms (6.79%) | Memory: 47.1 MB (6.81%) - LeetSync

---
+++ 1460-number-of-substrings-containing-all-three-characters/number-of-substrings-containing-all-three-characters.java
@@ -0,0 +1,29 @@
+class Solution {
+    public int numberOfSubstrings(String s) {
+
+        int n = s.length();
+
+        int left =0;
+        int count=0;
+        HashMap<Character,Integer> map = new HashMap<>();
+
+        for(int right=0;right<n;right++){
+
+            map.put(s.charAt(right),map.getOrDefault(s.charAt(right),0)+1);
+
+            while(map.size()==3){
+
+                count+= n - right;
+
+                map.put(s.charAt(left),map.get(s.charAt(left))-1);
+                if(map.get(s.charAt(left))==0){
+                    map.remove(s.charAt(left));
+                }
+                left++;
+            }
+        }
+
+        return count;
+        
+    }
+}
