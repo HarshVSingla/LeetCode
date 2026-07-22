2a426f92ea3d98565e974a4e906727ad667b7582
Harsh Singla <imharshsingla@gmail.com>
2026-07-22 11:31:56 +0530

    Time: 34 ms (71.55%) | Memory: 48.2 MB (45.43%) - LeetSync

--- 2470-removing-stars-from-a-string/removing-stars-from-a-string.java
+++ 2470-removing-stars-from-a-string/removing-stars-from-a-string.java
@@ -1,26 +1,43 @@
 class Solution {
     public String removeStars(String s) {
 
-        Deque<Character> st = new ArrayDeque<>();
+        // Deque<Character> st = new ArrayDeque<>();
+        // StringBuilder ans = new StringBuilder();
+
+        // for(int i=0;i<s.length();i++){
+
+        //     if(s.charAt(i)=='*'){
+        //         st.pop();
+        //     }
+        //     else{
+        //         st.push(s.charAt(i));
+        //     }
+        // }
+
+        // while(!st.isEmpty()){
+        //     ans.append(st.pop());
+        // }
+        // ans.reverse();
+
+        // String ak = ans.toString();
+        // return ak; 
+
+
+        // 2nd approach:
+
         StringBuilder ans = new StringBuilder();
 
-        for(int i=0;i<s.length();i++){
+        for(int i = 0;i<s.length();i++){
 
             if(s.charAt(i)=='*'){
-                st.pop();
+                ans.deleteCharAt(ans.length()-1);
             }
             else{
-                st.push(s.charAt(i));
+                ans.append(s.charAt(i));
             }
         }
 
-        while(!st.isEmpty()){
-            ans.append(st.pop());
-        }
-        ans.reverse();
-
-        String ak = ans.toString();
-        return ak; 
+        return ans.toString();
 
 
     }
