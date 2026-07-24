53038b5ae912a515c3d4c93e3edcfbea3fb7c855
Harsh Singla <imharshsingla@gmail.com>
2026-07-24 23:22:50 +0530

    Time: 7 ms (42.33%) | Memory: 43.5 MB (35.67%) - LeetSync

--- 2195-time-needed-to-buy-tickets/time-needed-to-buy-tickets.java
+++ 2195-time-needed-to-buy-tickets/time-needed-to-buy-tickets.java
@@ -1,23 +1,52 @@
 class Solution {
     public int timeRequiredToBuy(int[] tickets, int k) {
         
-        int a = 0;
+        // 1st approach -> Using only array
+        // int a = 0;
+        // int count=0;
 
-        int count=0;
+        // int n = tickets.length;
+        // while(tickets[k]!=0){
+        //     if(tickets[a]!=0){
+        //         tickets[a]-= 1;
+        //         count++;
+        //     }
+        //     a = (a+1)%n;
+        // }
+        // return count;        
+
+        
+        // 2nd approach -> Using queue 
+
+        Deque<int[]> q = new ArrayDeque<>();
 
         int n = tickets.length;
 
-        while(tickets[k]!=0){
-
-            if(tickets[a]!=0){
-                tickets[a]-= 1;
-                count++;
-            }
-
-            a = (a+1)%n;
+        for(int i =0;i<n;i++){
+            q.offer(new int[]{i,tickets[i]});
         }
 
-        return count;        
+        int count=0;
+
+        while(!q.isEmpty()){
+
+            int[] person = q.poll();
+
+            person[1]--;
+            count++;
+            // 1 <= tickets[i] <= 100 , element cannot be already 0.
+
+            if(person[1]==0){
+                if(person[0]==k){
+                    return count;
+                }
+            }
+            else{
+                q.offer(person);
+            }
+        }
+
+        return count;
 
     }
 }
