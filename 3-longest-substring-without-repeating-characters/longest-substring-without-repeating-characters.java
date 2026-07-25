091e721b6224f69f4d7f9e69c3b233d9a8be7ac0
Harsh Singla <imharshsingla@gmail.com>
2026-07-25 19:34:47 +0530

    Time: 9 ms (14.53%) | Memory: 46.2 MB (59.21%) - LeetSync

---
+++ 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.java
@@ -0,0 +1,29 @@
+class Solution {
+    public int lengthOfLongestSubstring(String s) {
+
+        HashMap<Character,Integer> map = new HashMap<>();
+
+        int max=0;
+
+        int left=0;
+
+        for(int right=0;right<s.length();right++){
+            map.put(s.charAt(right),map.getOrDefault(s.charAt(right),0)+1);
+
+
+            while(map.get(s.charAt(right))>1){
+
+                map.put(s.charAt(left),map.get(s.charAt(left))-1);
+                if(map.get(s.charAt(left))==0){
+                    map.remove(s.charAt(left));
+                }
+                left++;
+            }
+
+            max = Math.max(max,right-left+1);
+        }
+
+        return max;      
+        
+    }
+}
