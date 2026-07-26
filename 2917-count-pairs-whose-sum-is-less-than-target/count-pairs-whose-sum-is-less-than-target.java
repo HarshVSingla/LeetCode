57f4b5f5c38c3cfda2890b2fb2b818532c64f92b
Harsh Singla <imharshsingla@gmail.com>
2026-07-26 15:06:46 +0530

    Time: 2 ms (97.82%) | Memory: 44.1 MB (66.53%) - LeetSync

--- 2917-count-pairs-whose-sum-is-less-than-target/count-pairs-whose-sum-is-less-than-target.java
+++ 2917-count-pairs-whose-sum-is-less-than-target/count-pairs-whose-sum-is-less-than-target.java
@@ -1,23 +1,50 @@
 class Solution {
     public int countPairs(List<Integer> nums, int target) {
         
+        // 1st approach - most optimal 
+        // Collections.sort(nums);
+        // int left =0;
+        // int right = nums.size() -1;
+        // int count=0;
+        // while(left<right){
+        //     int x = nums.get(right) + nums.get(left);
+        //     if(x>=target){
+        //         right--;
+        //     }
+        //     else if(x<target){
+        //         count+= right-left;
+        //         left++;
+        //     }
+        // }
+        // return count;
+
+
+        // 2nd approach - binary search 
+
         Collections.sort(nums);
 
-        int left =0;
-        int right = nums.size() -1;
+        int n = nums.size();
         int count=0;
 
-        while(left<right){
-            int x = nums.get(right) + nums.get(left);
+        for(int i=0;i<n-1;i++){
+            int left = i+1;
+            int right = n-1;
+            int valid = i;
 
-            if(x>=target){
-                right--;
-            }
-            else if(x<target){
-                count+= right-left;
-                left++;
+            while(left<=right){
+
+                int mid = left + (right-left)/2;
+
+                if(nums.get(mid)+nums.get(i)<target){
+                    valid = mid;
+                    left = mid+1;
+                }
+                else{
+                    right = mid-1;
+                }
             }
 
+            count+= valid-i;
         }
 
         return count;
