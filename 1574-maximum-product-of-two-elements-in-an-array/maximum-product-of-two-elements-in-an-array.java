f2176b06a1e3cb1f1d30fd73649540f2cae612b3
Harsh Singla <imharshsingla@gmail.com>
2026-07-27 12:32:23 +0530

    Time: 1 ms (66.20%) | Memory: 44.5 MB (73.61%) - LeetSync

--- 1574-maximum-product-of-two-elements-in-an-array/maximum-product-of-two-elements-in-an-array.java
+++ 1574-maximum-product-of-two-elements-in-an-array/maximum-product-of-two-elements-in-an-array.java
@@ -1,22 +1,22 @@
 class Solution {
     public int maxProduct(int[] nums) {
         
-        // int max = nums[0];
-        // int sec = Integer.MIN_VALUE;
+        int max = nums[0];
+        int sec = Integer.MIN_VALUE;
 
-        // for(int i=1;i<nums.length;i++){
+        for(int i=1;i<nums.length;i++){
 
-        //     if(nums[i]>=max){
-        //         sec  = max;
-        //         max = nums[i];
-        //     }
+            if(nums[i]>=max){
+                sec  = max;
+                max = nums[i];
+            }
 
-        //     if(nums[i]<max && nums[i]>sec){
-        //         sec = nums[i];
-        //     }
-        // }
+            if(nums[i]<max && nums[i]>sec){
+                sec = nums[i];
+            }
+        }
 
-        // return (max-1)*(sec-1);
+        return (max-1)*(sec-1);
 
 
         // 2nd approach
@@ -28,15 +28,15 @@ class Solution {
 
         // 3rd approach: Using Priority queue
 
-        PriorityQueue<Integer> pq = new PriorityQueue<>(Collections.reverseOrder());
+        // PriorityQueue<Integer> pq = new PriorityQueue<>(Collections.reverseOrder());
 
-        for(int i=0;i<nums.length;i++){
-            pq.offer(nums[i]);
-        }
+        // for(int i=0;i<nums.length;i++){
+        //     pq.offer(nums[i]);
+        // }
 
-        int max = pq.poll();
-        int sec = pq.poll();
-        return (max-1) * (sec-1);
+        // int max = pq.poll();
+        // int sec = pq.poll();
+        // return (max-1) * (sec-1);
 
     }
 }
