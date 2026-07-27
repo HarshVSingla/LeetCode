01a8e3e4560ec5b660232c04a4d73bd0b8153231
Harsh Singla <imharshsingla@gmail.com>
2026-07-27 12:32:04 +0530

    Time: 3 ms (48.80%) | Memory: 45 MB (24.40%) - LeetSync

--- 1574-maximum-product-of-two-elements-in-an-array/maximum-product-of-two-elements-in-an-array.java
+++ 1574-maximum-product-of-two-elements-in-an-array/maximum-product-of-two-elements-in-an-array.java
@@ -1,22 +1,22 @@
 class Solution {
     public int maxProduct(int[] nums) {
         
-        int max = nums[0];
-        int sec = Integer.MIN_VALUE;
+        // int max = nums[0];
+        // int sec = Integer.MIN_VALUE;
 
-        for(int i=1;i<nums.length;i++){
+        // for(int i=1;i<nums.length;i++){
 
-            if(nums[i]>=max){
-                sec  = max;
-                max = nums[i];
-            }
+        //     if(nums[i]>=max){
+        //         sec  = max;
+        //         max = nums[i];
+        //     }
 
-            if(nums[i]<max && nums[i]>sec){
-                sec = nums[i];
-            }
-        }
+        //     if(nums[i]<max && nums[i]>sec){
+        //         sec = nums[i];
+        //     }
+        // }
 
-        return (max-1)*(sec-1);
+        // return (max-1)*(sec-1);
 
 
         // 2nd approach
@@ -24,6 +24,19 @@ class Solution {
         // Arrays.sort(nums);
         // int n = nums.length;
         // return (nums[n-1] -1)*(nums[n-2]-1);
+
+
+        // 3rd approach: Using Priority queue
+
+        PriorityQueue<Integer> pq = new PriorityQueue<>(Collections.reverseOrder());
+
+        for(int i=0;i<nums.length;i++){
+            pq.offer(nums[i]);
+        }
+
+        int max = pq.poll();
+        int sec = pq.poll();
+        return (max-1) * (sec-1);
 
     }
 }
