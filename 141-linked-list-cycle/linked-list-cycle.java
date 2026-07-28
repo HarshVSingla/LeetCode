bc21392da4fdf2442914900b328f4ba24cbe7706
Harsh Singla <imharshsingla@gmail.com>
2026-07-28 12:43:07 +0530

    Time: 7 ms (10.02%) | Memory: 46.9 MB (33.60%) - LeetSync

--- 141-linked-list-cycle/linked-list-cycle.java
+++ 141-linked-list-cycle/linked-list-cycle.java
@@ -13,19 +13,36 @@ public class Solution {
     public boolean hasCycle(ListNode head) {
 
 
-        if(head==null || head.next==null){
-            return false;
-        }
+        // if(head==null || head.next==null){
+        //     return false;
+        // }
 
-        ListNode slow = head;
-        ListNode fast = head;
+        // ListNode slow = head;
+        // ListNode fast = head;
 
-        while(fast!=null && fast.next!=null){
-            slow = slow.next;
-            fast = fast.next.next;
+        // while(fast!=null && fast.next!=null){
+        //     slow = slow.next;
+        //     fast = fast.next.next;
 
 
-            if(fast==slow){
+        //     if(fast==slow){
+        //         return true;
+        //     }
+        // }
+
+        // return false;
+
+
+        HashSet<ListNode> set = new HashSet<>();
+
+        ListNode temp = head;
+
+        while(temp!=null){
+            if(!set.contains(temp)){
+                set.add(temp);
+                temp = temp.next;
+            }
+            else{
                 return true;
             }
         }
