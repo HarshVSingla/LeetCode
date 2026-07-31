044404ea8e97adf5ebc40cdc170d5e60a28433bd
Harsh Singla <imharshsingla@gmail.com>
2026-07-31 15:08:16 +0530

    Time: 0 ms (100.00%) | Memory: 44.4 MB (96.89%) - LeetSync

--- 2210-find-target-indices-after-sorting-array/find-target-indices-after-sorting-array.java
+++ 2210-find-target-indices-after-sorting-array/find-target-indices-after-sorting-array.java
@@ -1,42 +1,65 @@
 class Solution {
     public List<Integer> targetIndices(int[] nums, int target) {
         
+        // List<Integer> arr = new ArrayList<>();
+
+        // Arrays.sort(nums);
+
+        // int right = nums.length -1;
+        // int left =0;
+
+        // while(left<=right){
+        //     int mid = left+ (right-left)/2;
+        //     int mids = mid;
+
+        //     if(nums[mid]>target){
+        //         right = mid-1;
+        //     }
+        //     else if(nums[mid]<target){
+        //         left = mid+1;
+
+        //     }
+        //     else{
+
+        //         while(mid>=1 && nums[mid-1]==target){
+        //             mid--;
+        //         }
+        //         while(mids<nums.length-1 && nums[mids+1]==target){
+        //             mids++;
+        //         }
+
+        //         for(int i = mid;i<=mids;i++){
+        //             arr.add(i);
+        //         }
+
+        //         break; 
+
+        //     }
+        // }
+
+        // return arr;
+
+
+        // 2nd approach - Optimal Way
         List<Integer> arr = new ArrayList<>();
 
-        Arrays.sort(nums);
+        int equal = 0;
+        int smaller = 0;
 
-        int right = nums.length -1;
-        int left =0;
-
-        while(left<=right){
-            int mid = left+ (right-left)/2;
-            int mids = mid;
-
-            if(nums[mid]>target){
-                right = mid-1;
+        for(int x : nums){
+            if(x==target){
+                equal++;
             }
-            else if(nums[mid]<target){
-                left = mid+1;
-
-            }
-            else{
-
-                while(mid>=1 && nums[mid-1]==target){
-                    mid--;
-                }
-                while(mids<nums.length-1 && nums[mids+1]==target){
-                    mids++;
-                }
-
-                for(int i = mid;i<=mids;i++){
-                    arr.add(i);
-                }
-
-                break; 
-
+            else if(x<target){
+                smaller++;
             }
         }
 
+        for(int i=0;i<equal;i++){
+            arr.add(smaller+i);
+        }
+
         return arr;
+
     }
 }
