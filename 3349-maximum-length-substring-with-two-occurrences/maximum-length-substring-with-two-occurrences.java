68791bd97d53723dee5c549b6300d27a321811a5
Harsh Singla <imharshsingla@gmail.com>
2026-08-22 14:45:04 +0530

    Time: 1 ms (100.00%) | Memory: 43.1 MB (96.58%) - LeetSync

--- 3349-maximum-length-substring-with-two-occurrences/maximum-length-substring-with-two-occurrences.java
+++ 3349-maximum-length-substring-with-two-occurrences/maximum-length-substring-with-two-occurrences.java
@@ -1,32 +1,57 @@
 class Solution {
     public int maximumLengthSubstring(String s) {
         
+        // int n = s.length();
+        // int left =0;
+
+        // int max = 0;
+
+        // HashMap<Character,Integer> map = new HashMap<>();
+
+
+        // for(int right =0;right<n;right++){
+
+        //     map.put(s.charAt(right),map.getOrDefault(s.charAt(right),0)+1);
+            
+
+        //     while(map.get(s.charAt(right))>2){
+        //         map.put(s.charAt(left),map.get(s.charAt(left))-1);
+        //         if(map.get(s.charAt(left))==0){
+        //             map.remove(s.charAt(left));
+        //         }
+        //         left++;
+                
+        //     }
+
+        //     max = Math.max(max,right-left+1);
+        // }
+
+        // return max;
+
+        // optimal way - array:
+
+
         int n = s.length();
-        int left =0;
+        int[] map = new int[26];
 
+        int left = 0;
         int max = 0;
-
-        HashMap<Character,Integer> map = new HashMap<>();
-
 
         for(int right =0;right<n;right++){
 
-            map.put(s.charAt(right),map.getOrDefault(s.charAt(right),0)+1);
-            
+            int rightchar = s.charAt(right)-'a';
+            map[rightchar]++;
 
-            while(map.get(s.charAt(right))>2){
-                map.put(s.charAt(left),map.get(s.charAt(left))-1);
-                if(map.get(s.charAt(left))==0){
-                    map.remove(s.charAt(left));
-                }
+            while(map[rightchar]>2){
+                int leftchar = s.charAt(left)-'a';
+                map[leftchar]--;
+
                 left++;
-                
             }
 
             max = Math.max(max,right-left+1);
         }
 
         return max;
-
     }
 }
