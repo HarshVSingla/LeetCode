8fadf0f744dce7f361100c2145d93386ef247bda
Harsh Singla <imharshsingla@gmail.com>
2026-09-14 14:27:30 +0530

    Time: 1 ms (99.92%) | Memory: 45.4 MB (65.34%) - LeetSync

--- 443-string-compression/string-compression.java
+++ 443-string-compression/string-compression.java
@@ -1,33 +1,67 @@
 class Solution {
     public int compress(char[] chars) {
 
-        String ans = "";
+        // String ans = "";
+        // int n = chars.length;
+
+        // int i =0;
+        
+        // while(i<n){
+
+        //     char ch = chars[i];
+        //     int count=0;
+
+
+        //     while(i<n && chars[i]==ch){
+        //         i++;
+        //         count++;
+        //     }
+        //     ans+= ch;
+        //     if(count>1){
+        //         ans+= count;
+        //     }
+        // }
+
+        // int size = ans.length();
+        // for(int a =0;a<size;a++){
+        //     chars[a] = ans.charAt(a);
+        // }
+
+        // return size;
+
+
+        // OPTIMISED APPROACH (O(N))
+
+        int read = 0;
+        int write = 0;
         int n = chars.length;
 
-        int i =0;
-        
-        while(i<n){
+        while(read<n){
 
-            char ch = chars[i];
             int count=0;
+            char ch = chars[read];
 
-
-            while(i<n && chars[i]==ch){
-                i++;
+            while(read<n && chars[read]==ch){
+                read++;
                 count++;
             }
-            ans+= ch;
+
+            chars[write++] = ch;
+
             if(count>1){
-                ans+= count;
+
+                String s = String.valueOf(count);
+
+                for(int i=0;i<s.length();i++){
+                    chars[write++] = s.charAt(i);
+                }
             }
-        }
 
-        int size = ans.length();
-        for(int a =0;a<size;a++){
-            chars[a] = ans.charAt(a);
-        }
 
-        return size;
+        } 
+
+        return write;          
+
 
         
     }
